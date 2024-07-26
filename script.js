4cceffc684123f8eabf7137a54e2d01f4b1fc538
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-27 00:01:03 +0400

    fix the displays and divide by 0

--- script.js
+++ script.js
@@ -1,22 +1,27 @@
 const buttons = document.querySelectorAll('.number');
-let wholeOp = document.querySelector('.whole-operation');
+const wholeOp = document.querySelector('.whole-operation');
 const currentOp = document.querySelector('.current-operation');
 const equalBtn = document.querySelector('.equals');
 const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete')
 let operator = document.querySelectorAll('.operation');
 let expression = '';
+const operatorArr = ['+', '-', '/', '*'];
+clearBtn.addEventListener('click', clear);
 
 function getNumbers() {
   deleteN()
   buttons.forEach((element) => 
     element.addEventListener('click', (el) => {
-      const lastChar = expression.slice(-1);
+      console.log(expression)
+      console.log(currentOp.textContent)
+      console.log(wholeOp.textContent)
+      console.log(typeof expression)
+      const lastChar = expression.slice(-2);
       if (isNaN(parseInt(lastChar))) {
         currentOp.textContent = '';
-      } else if (lastChar === ' =')
-      {
-        wholeOp.textContent = el.target.textContent
+      } else if (lastChar === ' =') {
+          wholeOp.textContent = el.target.textContent
       }
 
       currentOp.textContent += el.target.textContent;
@@ -31,10 +36,22 @@ function equals() {
     let [firstNum, operator, secondNum] = expression.split(" ")
     firstNum = parseFloat(firstNum); 
     secondNum = parseFloat(secondNum); 
-    let result = operate(firstNum, operator, secondNum);
-    currentOp.textContent =  result; 
-    expression = result;
-    wholeOp.textContent += ' ='
+    console.log(firstNum)
+    console.log(secondNum)
+    console.log(operator)
+    console.log(expression)
+    if (expression.split(" ").length < 3 || isNaN(secondNum)) {
+      return;
+    }
+    if ((operator == '/') && (secondNum == '0')) {
+      alert("HEY you cant divide a number by 0");
+      clear();
+    } else {
+      let result = operate(firstNum, operator, secondNum);
+      currentOp.textContent =  result; 
+      expression = result;
+      wholeOp.textContent += ' =';
+    }
   })
 }
 
@@ -49,17 +66,22 @@ function getOperator() {
 }
 
 function clear() {
-  clearBtn.addEventListener('click', () => {
     expression = '';
     currentOp.textContent = '';
     wholeOp.textContent = '';
-  })
 }
 
 function deleteN() {
   deleteBtn.addEventListener('click', () => {
-    currentOp.textContent = currentOp.textContent.slice(0, -1);
-    expression = currentOp.textContent;
+    if (wholeOp.textContent.charAt(wholeOp.textContent.length -1 ) === '=') {
+      currentOp.textContent = currentOp.textContent.slice(0, -1);
+      expression = currentOp.textContent;
+      console.log(expression)
+    } else {
+      currentOp.textContent = currentOp.textContent.slice(0, -1);
+      expression = expression.slice(0, -1);
+      wholeOp.textContent = wholeOp.textContent.slice(0, -1);
+    }
   })
 }
 
@@ -78,7 +100,6 @@ function operate(firstNum, operator, secondNum) {
       break;
     case '/':
       result = divideNumbers(firstNum, secondNum);
-      break;
   }
   return result
 }
@@ -99,9 +120,6 @@ function divideNumbers(a, b) {
   return a / b;
 }
 
-console.log(operate());
-
 getNumbers();
 equals();
 getOperator();
-clear();
