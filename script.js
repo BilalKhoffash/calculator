13373f073b148ca27af308290f6ff07c66c355e8
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-26 17:13:54 +0400

    give logic to numbers and clear

--- script.js
+++ script.js
@@ -1,11 +1,54 @@
-const operation = '8 + 9'; // This is just an example
-let [firstNum, operator, secondNum] = operation.split(" ");
+// const operation = '8 + 9'; // This is just an example
+// let [firstNum, operator, secondNum] = operation.split(" ");
+const buttons = document.querySelectorAll('.number');
+let wholeOp = document.querySelector('.whole-operation');
+const currentOp = document.querySelector('.current-operation');
+const equalBtn = document.querySelector('.equals');
+const clearBtn = document.querySelector('.clear');
+let operator = document.querySelectorAll('.operation');
+let expression = '';
 
-function operate() {
+function getNumbers() {
+  buttons.forEach((element) => 
+    element.addEventListener('click', (el) => {
+      currentOp.textContent += el.target.textContent;
+      expression += el.target.textContent;
+    })
+  )
+}
+
+function equals() {
+  equalBtn.addEventListener('click', () => {
+    let [firstNum, operator, secondNum] = expression.split(" ")
+    firstNum = parseFloat(firstNum); 
+    secondNum = parseFloat(secondNum); 
+    let result = operate(firstNum, operator, secondNum);
+    currentOp.textContent =  result; 
+    expression = result;
+  })
+}
+
+function getOperator() {
+  operator.forEach((element) => 
+    element.addEventListener('click', (el) => {
+      operator = el.target.textContent;
+      expression += ' ' + el.target.textContent + ' ';
+      console.log('test');
+      currentOp.textContent =  ''; 
+    })
+  )
+}
+
+function clear() {
+  clearBtn.addEventListener('click', () => {
+    expression = '';
+    currentOp.textContent = '';
+  })
+}
+
+function operate(firstNum, operator, secondNum) {
   let result;
-  firstNum = parseFloat(firstNum); 
-  secondNum = parseFloat(secondNum);
-
+  
   switch(operator) {
     case '+':
       result = addNumbers(firstNum, secondNum);
@@ -40,3 +83,8 @@ function divideNumbers(a, b) {
 }
 
 console.log(operate());
+
+getNumbers();
+equals();
+getOperator();
+clear();
