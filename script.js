9f8e5e2dd4f300eb48dd53a923c21d36602ee47c
BilalKhoffash <goldenpotatoes.1204@gmail.com>
2024-07-27 19:49:06 +0400

    fix delete button and make display better

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const clearBtn = document.querySelector('.clear');
 const deleteBtn = document.querySelector('.delete')
 let operator = document.querySelectorAll('.operation');
 let expression = '';
-let test1 = ''; 
 const operatorArr = ['+', '-', '/', '*'];
 clearBtn.addEventListener('click', clear);
 
@@ -17,29 +16,23 @@ function getNumbers() {
       const lastChar = String(expression).slice(-2, -1);
       if (operatorArr.includes(lastChar)) {
         currentOp.textContent = '';
-        currentOp.textContent += el.target.textContent;
-        expression += el.target.textContent;
-        wholeOp.textContent += el.target.textContent
-      } else if (wholeOp.textContent.slice(-2, -1) == '=') {
-        currentOp.textContent += el.target.textContent;
-        expression += el.target.textContent;
-      } else {
-        currentOp.textContent += el.target.textContent;
-        expression += el.target.textContent;
-        wholeOp.textContent += el.target.textContent
-      }
+      } 
+      currentOp.textContent += el.target.textContent;
+      expression += el.target.textContent;
     })
   )
 }
 
 function equals() {
   equalBtn.addEventListener('click', () => {
+    if (wholeOp.textContent.includes('=')) return;
     let [firstNum, operator, secondNum] = expression.split(" ")
     firstNum = parseFloat(firstNum); 
     secondNum = parseFloat(secondNum); 
     if (expression.split(" ").length < 3 || isNaN(secondNum)) {
       return;
     }
+    wholeOp.textContent += currentOp.textContent;
     if ((operator == '/') && (secondNum == '0')) {
       alert("HEY you cant divide a number by 0");
       clear();
@@ -57,10 +50,11 @@ function getOperator() {
   operator.forEach((element) => 
     element.addEventListener('click', (el) => {
       if (expression === '') return;
-      if (operatorArr.some(op => expression.includes(`${op}`))) return;
+      if (operatorArr.some(op => expression.toString().includes(`${op}`))) return;
       operator = el.target.textContent;
       expression += ' ' + el.target.textContent + ' ';
-      wholeOp.textContent = expression
+      wholeOp.textContent = expression;
+      currentOp.textContent = "Enter a number";
     })
   )
 }
@@ -73,13 +67,15 @@ function clear() {
 
 function deleteN() {
   deleteBtn.addEventListener('click', () => {
-    if (wholeOp.textContent.charAt(wholeOp.textContent.length - 2 ) === '=') {
+    const check = wholeOp.textContent.charAt(wholeOp.textContent.length - 2)
+    if (check === '=') {
       currentOp.textContent = currentOp.textContent.slice(0, -1);
       expression = currentOp.textContent;
+    } else if (operatorArr.includes(check) && !(Number(currentOp.textContent))){
+      return;
     } else {
       currentOp.textContent = currentOp.textContent.slice(0, -1);
       expression = expression.slice(0, -1);
-      wholeOp.textContent = wholeOp.textContent.slice(0, -1);
     }
   })
 }
@@ -122,4 +118,3 @@ function divideNumbers(a, b) {
 getNumbers();
 equals();
 getOperator();
-clear();
